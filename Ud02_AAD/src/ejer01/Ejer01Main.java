e0f57a49689b265ae2dc76b16b45dd4ecbfa7a61
DAM2Alu5 <DAM2Alu5@B01-PC11.IESGUARA.LOCAL>
2022-09-16 13:32:57 +0200

    2022-09-16 13:32 AAD
    Realizado ejercicio 02 de la ud2

--- Ud02_AAD/src/ejer01/Ejer01Main.java
+++ Ud02_AAD/src/ejer01/Ejer01Main.java
@@ -3,7 +3,9 @@ package ejer01;
 import java.io.File;
 
 public class Ejer01Main {
-
+	/*UD2.1 Realiza una clase UD2_1 que muestre nombre, longitud, si se puede leer, si se puede
+	escribir de todos los archivos ocultos de la carpeta Windows de tu disco; sólo de los que se
+	encuentran en la carpeta principal de Windows, no en sus subcarpetas.*/
 	public static void main(String[] args) {
 		File file = new File ("c:\\windows");
 		System.out.println("Ruta :" + file.getPath());
--- Ud02_AAD/src/ejer02/Ejer02Main.java
+++ Ud02_AAD/src/ejer02/Ejer02Main.java
@@ -1,9 +1,37 @@
 package ejer02;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Ejer02Main {
-
+	/*Realiza una clase UD2_2 cuyo método principal reciba como argumento una cadena con
+	la trayectoria de un directorio o fichero e indique si existe realmente o no dicho directorio o
+	fichero. Si el método principal no recibe ningún argumento se indicará por pantalla y finalizará
+	su ejecución.*/
+	
+	public static boolean existe(String ruta) {
+		Boolean existe = false;
+		File f = new File(ruta);
+		
+		if (f.isDirectory() || f.isFile()) {
+			existe = true;
+		}
+		
+		return existe;
+	}
+	
 	public static void main(String[] args) {
-
+		Scanner s = new Scanner(System.in);
+		if (args.length!=0) {
+			String ruta = args[0];
+			System.out.print("Existe: ");
+			System.out.println(existe(ruta));
+		} else {
+			System.out.println("No ha introducido argumentos");
+			System.exit(-1);
+		}
+		
+		
 	}
 
 }
