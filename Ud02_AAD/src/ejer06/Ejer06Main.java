78d3a301bf102ffa572067a5798fdcf9fae23f66
DAM2Alu5 <DAM2Alu5@B01-PC11.IESGUARA.LOCAL>
2022-09-23 13:31:02 +0200

    2022-09-23 13:30 Clase AAD
    Se copian los ejercicios del 6 al 8

--- Ud02_AAD/src/ejer06/Ejer06Main.java
+++ Ud02_AAD/src/ejer06/Ejer06Main.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Ejer06Main {
 	
@@ -15,35 +16,72 @@ public class Ejer06Main {
 //	Pistas:
 //	- Métodos para cadenas substring e indexOf.
 	
+//	public static void main(String[] args) {
+//		
+//		try {
+//			BufferedReader br = new BufferedReader(new FileReader("Ficheros\\" + args[0]));
+//			ArrayList<String>palabras = new ArrayList<String>();
+//			int palNum = 0;
+//			String linea;
+//			
+//			while((linea = br.readLine())!=null) {
+//				palabras.add(linea);
+//			}
+//			
+//			br.close();
+//			
+//			for(String palabra : palabras) {
+//				if (palabra.equalsIgnoreCase(args[1])) {
+//					palNum++;
+//				}
+//			}
+//			
+//			System.out.println("El número de veces que la palabra " + args[1] + " está en este fichero es de: " + palNum);
+//			
+//		} catch (FileNotFoundException e) {
+//			System.err.println("No se ha encontrado el fichero " + args[1]);
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+	
+	static Scanner s = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("Ficheros\\" + args[0]));
-			ArrayList<String>palabras = new ArrayList<String>();
-			int palNum = 0;
+			System.out.print("Pon una palabra a buscar en el texto: ");
+			String palabra_a_buscar = s.next();
+			BufferedReader br = new BufferedReader(new FileReader("Ficheros\\lorem01"));
 			String linea;
-			
-			while((linea = br.readLine())!=null) {
-				palabras.add(linea);
+			String texto = "";
+			int contador = 0;
+			while ((linea = br.readLine()) != null) {
+				texto += linea + " ";
 			}
-			
-			br.close();
-			
-			for(String palabra : palabras) {
-				if (palabra.equalsIgnoreCase(args[1])) {
-					palNum++;
+			StringTokenizer st = new StringTokenizer(texto, " ");
+			while (st.hasMoreTokens()) {
+				if (st.nextToken().equalsIgnoreCase(palabra_a_buscar)) {
+					contador++;
 				}
 			}
-			
-			System.out.println("El número de veces que la palabra " + args[1] + " está en este fichero es de: " + palNum);
-			
+			if (contador == 0) {
+				System.out.println("No se ha encontrado '" + palabra_a_buscar + "' en el texto.");
+			} else if (contador == 1) {
+				System.out.println("La palabra '" + palabra_a_buscar + "' se ha encontrado " + contador + " vez.");
+			} else {
+				System.out.println("La palabra '" + palabra_a_buscar + "' se ha encontrado " + contador + " veces.");
+			}
 		} catch (FileNotFoundException e) {
-			System.err.println("No se ha encontrado el fichero " + args[1]);
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
+
 
 }
