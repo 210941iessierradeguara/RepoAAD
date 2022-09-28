23eb689e5132a49c34a2bb903367cc2d0c9925a2
DAM2Alu5 <DAM2Alu5@B01-PC11.IESGUARA.LOCAL>
2022-09-28 09:07:06 +0200

    test

--- Ud02_AAD/src/ejer09/Ejer09Main.java
+++ Ud02_AAD/src/ejer09/Ejer09Main.java
@@ -8,7 +8,6 @@ import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 class Ejer09Main {
-	
 //	Adaptación de los ejemplos vistos en los materiales con la clase RandomAccessFile.
 //	Realiza una clase UD2_9 que pida al usuario el identificador del profesor y lo borre del fichero
 //	ProfesFPSierraGuara.dat. Borrar un dato simplemente consiste en poner su campo id dentro del
