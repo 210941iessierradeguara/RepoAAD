897486d9e4febb3d19da8aadae9e568033b97c8b
DAM2Alu5 <DAM2Alu5@B01-PC11.IESGUARA.LOCAL>
2022-10-19 12:13:07 +0200

    2022-10-19 Anexo III
    Se hace lo del anexo 3 pero no tira por de pronto. Además se añade la
    UD03 a github porque me había olvidado de hacerlo.

---
+++ UD03/src/anexoIII_IV/AccesoBd.java
@@ -0,0 +1,93 @@
+package anexoIII_IV;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccesoBd {
+
+	private static String driver = "com.mysql.cj.jdbc.Driver";
+	private static String database = "baloncesto";
+	private static String hostname = "localhost";
+	private static String port = "3306";
+	private static String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database
+			+ "?serverTimezone=Europe/Madrid";
+	private static String username = "root";
+	private static String password = "root";
+	static ResultSet reg;
+	private Connection conecta;
+
+	public void conectar() throws SQLException, ClassNotFoundException {
+		Class.forName(driver);
+		conecta = DriverManager.getConnection(url, username, password);
+	}
+
+	public void desconectar() throws SQLException {
+		if (conecta != null) {
+			conecta.close();
+		}
+	}
+
+	public ResultSet consulta(String localidad) throws SQLException {
+		CallableStatement consulta = null;
+		consulta = conecta.prepareCall("CALL consultaSocio(?)");
+		consulta.setString(1, localidad);
+		ResultSet reg = consulta.executeQuery();
+		return reg;
+	}
+
+	public ResultSet limpia() throws SQLException {
+		PreparedStatement consulta;
+		String cadenaSQL = "select * from socio where socioID=0";
+		consulta = conecta.prepareStatement(cadenaSQL, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+		ResultSet reg = consulta.executeQuery();
+		return reg;
+	}
+
+	public int nuevo(Socio socio) {
+		try {
+			String sql = "insert into socio values (((select max(socioID)from socio s)+1),?,?,?,?)";
+			PreparedStatement inserta = conecta.prepareStatement(sql);
+			inserta.setString(1, socio.getNombre());
+			inserta.setInt(2, socio.getEstatura());
+			inserta.setInt(3, socio.getEdad());
+			inserta.setString(4, socio.getLocalidad());
+			inserta.executeUpdate();
+			return 1;
+		} catch (SQLException e) {
+			return e.getErrorCode();
+		}
+	}
+
+	public int actualizar(Socio socio) {
+		try {
+			String sql = "update socio set nombre=?, estatura=?, edad=?, localidad=? where socioID=?";
+			PreparedStatement actualiza = conecta.prepareStatement(sql);
+			actualiza.setString(1, socio.getNombre());
+			actualiza.setInt(2, socio.getEstatura());
+			actualiza.setInt(3, socio.getEdad());
+			actualiza.setString(4, socio.getLocalidad());
+			actualiza.setInt(5, socio.getId());
+			return actualiza.executeUpdate();
+
+		} catch (SQLException e) {
+			return 0;
+		}
+
+	}
+
+	public int borrar(Socio socio) {
+		try {
+			String sql = "delete from socio where socioID=?";
+			PreparedStatement borra = conecta.prepareStatement(sql);
+			borra.setInt(1, socio.getId());
+			return borra.executeUpdate();
+		} catch (SQLException e) {
+			return 0;
+		}
+	}
+
+}
